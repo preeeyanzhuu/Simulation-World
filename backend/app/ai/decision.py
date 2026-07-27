c43b3b140d89a5ae731ed9ffff2b10a5d8045891
Priyanshu Singh Naruka <priyanshusinghnaruka@gmail.com>
2026-07-28 01:59:14 +0530

    Implement decision-making logic for Citizen actions

--- backend/app/ai/decision.py
+++ backend/app/ai/decision.py
@@ -1,1 +1,10 @@
-
+from app.models.citizen import Citizen
+def decide(citizen:Citizen):
+    if citizen.energy<30:
+        return "go_home"
+    elif citizen.hunger>70:
+        return "go_restaurant"
+    elif citizen.money<10:
+        return "go_work"
+    else:
+        return "idle"
