e9b7776ec28502dc580cb5069aab0d9963ff7165
Aditya Talan <adityatalan94@gmail.com>
2026-09-04 10:38:25 +0530

    Update city.py

--- backend/app/world/city.py
+++ backend/app/world/city.py
@@ -1,7 +1,7 @@
 GRID_SIZE = 20
 
 def create_grid():
-    grid = []       # grid[row][col]  →  row = y (vertical), col = x (horizontal)
+    grid = []       
     for row in range(GRID_SIZE):
         new_row =[]
         for col in range(GRID_SIZE):
@@ -9,12 +9,4 @@ def create_grid():
         grid.append(new_row)
     return grid 
 
-if __name__ == "__main__":
-    g = create_grid()
-    print(g[0])       # print just the first row
-    print(len(g))      # print how many rows total
 
-    g[5][5] = {"type": "road"}
-    g[10][10] = {"type": "building" , "name" : "hospital"}
-    print(g[5][5])
-    print(g[10][10])
