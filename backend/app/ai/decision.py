55ce1f62aba1f6912eacf1db48805b7588c15a73
Priyanshu Singh Naruka <priyanshusinghnaruka@gmail.com>
2026-09-04 10:23:24 +0530

    Update decision.py

--- backend/app/ai/decision.py
+++ backend/app/ai/decision.py
@@ -1,10 +1,18 @@
 from app.models.citizen import Citizen
+from app.ai.currency import get_currency
+
 def decide(citizen:Citizen):
+    currency = get_currency(citizen)
+
     if citizen.energy<30:
         return "go_home"
     elif citizen.hunger>70:
         return "go_restaurant"
-    elif citizen.money<10:
-        return "go_work"
+    elif citizen.happiness < 30:
+        return "go_mall"
+    elif currency < 10:
+        return "go_school" if citizen.occupation == "student" else "go_work"
+    
+
     else:
         return "idle"
