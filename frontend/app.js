807cd3a705c139da43beaacf36711251b9e30f35
Priyanshu Singh Naruka <priyanshusinghnaruka@gmail.com>
2026-09-12 22:48:00 +0530

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,17 @@
 
+import { connect } from "./services/socket.js";
+import * as townMap from "./components/townMap.js";
+import * as dashboard from "./components/dashboard.js";
+import * as navbar from "./components/navbar.js";
+import * as eventButtons from "./components/eventButtons.js";
+
+townMap.init();
+dashboard.init();
+eventButtons.init();
+
+connect((state) => {
+  townMap.setState(state);
+  dashboard.update(state);
+  navbar.update(state);
+  eventButtons.sync(state);
+});
